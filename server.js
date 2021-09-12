eb12e873fa9786652eab083caeba9ad4deaa5f4a
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2021-09-12 12:42:19 +0530

    deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app=express();
 
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}))
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: ['http://localhost:3000'] }));
 dotenv.config();
 const server=http.createServer(app);
 
